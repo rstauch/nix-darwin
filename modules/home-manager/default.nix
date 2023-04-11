183518a4bda4ad249ccd903740b060212ae914f4
Robert Stauch <rstauch@Roberts-MacBook-Air.local>
2023-04-11 12:05:09 +0200

    cleanup

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,34 +1,50 @@
-{ pkgs, ... }: {  
-    home.stateVersion = "22.11";
+{pkgs, ...}: let
+  PROJECT_ROOT = builtins.toString ./../../.;
+in {
+  home.stateVersion = "22.11";
 
-    home.packages = with pkgs; [
-        ripgrep
-        fd
-        curl
-        less
-    ];
-    home.sessionVariables = {
-        PAGER = "less";
-        CLICLOLOR = 1;
+  home.packages = with pkgs; [
+    ripgrep
+    fd
+    curl
+    less
+  ];
+  home.sessionVariables = {
+    PAGER = "less";
+    CLICLOLOR = 1;
+  };
+
+  # TODO: vollständige config einführen
+  programs = {
+    bat = {
+      enable = true;
+      config.theme = "TwoDark";
     };
 
-    # TODO: groupierung auf programs.x ebene
-    programs.bat.enable = true;
-    programs.bat.config.theme = "TwoDark";
-    programs.fzf.enable = true;
-    programs.fzf.enableZshIntegration = true;
-    programs.exa.enable = true;
-    programs.git.enable = true;
-    programs.zsh.enable = true;
-    programs.zsh.enableCompletion = true;
-    programs.zsh.enableAutosuggestions = true;
-    programs.zsh.enableSyntaxHighlighting = true;
-    programs.zsh.shellAliases = {
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    exa.enable = true;
+
+    git = {
+      enable = true;
+    };
+
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      enableSyntaxHighlighting = true;
+      shellAliases = {
         ls = "ls --color=auto -F";
 
-        # TODO: set path programatically
-        nixswitch = "darwin-rebuild switch --flake ~/projects/int/nix/.#";
-        nixup = "pushd ~/projects/int/nix; nix flake update; nixswitch; popd";
-    };
+        nixswitch = "darwin-rebuild switch --flake ${PROJECT_ROOT}/.#";
 
+        # scheint nur aus vscode terminal in projekt verzeichnis zu funktionieren ?
+        nixup = "pushd ${PROJECT_ROOT}; nix flake update; nixswitch; popd";
+      };
+    };
+  };
 }
