8cd325ca30fd3cb0aeb80b7df70f78066f945abb
Robert Stauch <robert.stauch@fluxdev.de>
2023-04-20 19:00:36 +0200

    wip

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,6 +1,9 @@
 # https://daiderd.com/nix-darwin/manual/index.html
 {pkgs, ...}: let
 in {
+  # TODO: ???
+  services.activate-system.enable = true;
+
   nix.settings.substituters = [
     "https://cache.nixos.org/"
   ];
@@ -9,7 +12,11 @@ in {
   ];
   programs.nix-index.enable = true;
 
-  # here go the darwin preferences and config items
+  nix.extraOptions = ''
+    auto-optimise-store = true
+    experimental-features = nix-command flakes
+  '';
+
   programs.zsh.enable = true;
   environment = {
     shells = with pkgs; [bash zsh];
@@ -23,27 +30,37 @@ in {
     systemPath = ["/opt/homebrew/bin"];
     pathsToLink = ["/Applications"];
   };
-  nix.extraOptions = ''
-    auto-optimise-store = true
-    experimental-features = nix-command flakes
-  '';
+
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToEscape = true;
   security.pam.enableSudoTouchIdAuth = true;
 
-  fonts.fontDir.enable = true; # DANGER
+  fonts.fontDir.enable = true;
   fonts.fonts = with pkgs; [
     nerdfonts
   ];
 
   services.nix-daemon.enable = true;
   system.defaults = {
-    # Dock
-    dock.autohide = false;
+    dock = {
+      autohide = false;
+
+      # TODO: Wert anpassen
+      tilesize = 64;
+    };
+    finder = {
+      # ???: CreateDesktop = false;
+      ShowPathbar = true;
+      ShowStatusBar = true;
+      AppleShowAllFiles = true;
+      AppleShowAllExtensions = true;
+      _FXShowPosixPathInTitle = true;
+    };
+    loginwindow = {
+      GuestEnabled = false;
+    };
 
     # Finder
-    finder.AppleShowAllExtensions = true;
-    finder._FXShowPosixPathInTitle = true;
     NSGlobalDomain.AppleShowAllExtensions = true;
     NSGlobalDomain.AppleShowAllFiles = true;
 
@@ -59,13 +76,40 @@ in {
     NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false; # spellcheck
     NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false; # smart-dash
     NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false; # smart-quotes
+    NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
     NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
 
-    # TODO: tinting = true tone, right click tap, highres
+    # TODO: tinting = true tone ?, right click tap, highres
 
     # https://github.com/mathiasbynens/dotfiles/blob/main/.macos
     # https://pawelgrzybek.com/change-macos-user-preferences-via-command-line/
   };
+
+  # TODO: UNGETESTED
+  # Disable the “Are you sure you want to open this application?” dialog
+  system.defaults.LaunchServices.LSQuarantine = false;
+
+  system.activationScripts.postActivation.text = ''
+    echo "Start setting up post activation settings ..."
+    # TODO: add and test settings
+
+    # TODO: Trackpad settings scheinen nicht zu funktionieren
+    # check with: defaults read com.apple.AppleMultitouchTrackpad
+    # Tap to click for this user and for the login screen
+    # defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
+    # defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+    # defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+    # Map bottom right corner to right-click
+    # defaults write com.apple.AppleMultitouchTrackpad TrackpadCornerSecondaryClick -int 2
+    # defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool false
+    # defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
+
+    # defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+
+    echo "Finished setting up post activation settings!"
+  '';
 
   system.stateVersion = 4;
 
@@ -85,7 +129,9 @@ in {
       "1password-cli"
       "firefox" # manual: login to sync settings, install addons: uBlockOrigin, darkreader, 1password
       "google-chrome"
-      "docker"
+      "docker" # manual: enable start automatically at login
+      "shottr"
+      # "betterdisplay" # TODO: requires license
     ];
     taps = ["homebrew/cask" "homebrew/cask-versions" "jbangdev/tap" "quarkusio/tap"];
     brews = ["jbang" "quarkus"];
