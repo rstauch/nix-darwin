5e701727b631c3f9e83702efc1f8c981498990ad
Stauch, R. (Robert) <robert.stauch@fluxdev.de>
2024-02-04 10:53:53 +0100

    update

--- modules/home-manager/k8s.nix
+++ modules/home-manager/k8s.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: let
 in {
   home.packages = with pkgs; [
-    #minikube
+    minikube
     kubectl
     kubernetes-helm
     # kubectx, https://github.com/ahmetb/kubectx
-    # k9s
+    k9s
     #kubeshark
   ];
 }
